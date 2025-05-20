e4d9c8f3fdc57e53169df0f8254c1e06db099445
betterbeup <betterbeup@gmail.com>
2025-05-20 11:41:08 -0400

    Add files via upload
    First day of Python programming and learning.

---
+++ Day_1_Python.py
@@ -0,0 +1,22 @@
+# This line of code is just an example of writing any words(Hello world, etc)
+# print("Hello world\nHello world\nHello World" + "\nMy name is ..." + " Frank")
+# This asks what is your name and gives you encouragement for a good job one day
+# print("Hello " + input("What is your name? ") + ", I hope you can learn how to code well enough to get a great job one day!")
+# name = input("What is your name?\n")
+# print("You will learn programming super fast, " + name + "!")
+# print("Your name is " + str(len(name)) + " numbers long.")
+# # switch the variables without using the same words
+# glass1 = "milk"
+# glass2 = "juice"
+# temporary = glass1
+# glass1 = glass2
+# glass2 = temporary
+# print(glass1 + " " + glass2)
+
+#Day 1 Project Band Name Generator
+print("Welcome to a Band name generator")
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name could be " + city + pet_name)
+
+
