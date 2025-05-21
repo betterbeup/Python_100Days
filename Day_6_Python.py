0d78ca69c84ae40ddb1c47b8a3935313c410dbdd
betterbeup <betterbeup@gmail.com>
2025-05-21 09:54:51 -0400

    Add files via upload
    Basic function, basic while loop

---
+++ Day_6_Python.py
@@ -0,0 +1,16 @@
+##Day_6_Python, functions and while loops
+#make own functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+#Calling the function
+my_function()
+#My first while statements
+random_number = 0
+while random_number < 5:
+    random_number += 1
+    print(f"Reverse countdown: {random_number}")
+while random_number > 0:
+    random_number -= 1
+    print(f"Countdown: {random_number}")
